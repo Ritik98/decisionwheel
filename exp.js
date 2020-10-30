097c4c8df4bd4d4fa6630fc5f2988544e0ad5def
Prarthana <prarthanashr04@gmail.com>
2020-10-30 07:36:56 +0530

    modified

--- exp.js
+++
@@ -1,7 +0,0 @@
-myobj= {
-    "choice" : " this is the choice ",  
-    "consequences" : "this is the consequences",
-    "value" : " this the value related to the choice youy made",
-    "feelings" : " this is the feeling for that choice "
-}
-
