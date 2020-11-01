f183df8099c05930e5272976ceb59e80c4be331a
Jatin <adriano23012001@gmail.com>
2020-11-01 13:28:36 +0530

    Support multiple data entries for choices,help

--- function.js
+++ function.js
@@ -1,14 +1,24 @@
 var App =
 {
-    "State" : {
-        "CurrentStage" : 0
+    "State": {
+        "CurrentStage": 0
     },
-    "UserData": ["","","","","","","","",""]
+    "UserData": [
+        "",
+        [],
+        [],
+        [],
+        [],
+        "",
+        [],
+        "",
+        ""
+    ]
 }
 
 var DataEntryPane =
 {
-    "Questions" : [
+    "Questions": [
         "What is Problem ?",
         "What are the choices?",
         "What are the consequences ?",
@@ -19,54 +29,67 @@ var DataEntryPane =
         "What is your Decision ?",
         "Assess Decision"
     ],
-    SubmitInput()
-    {
-      var response = $.trim($("#iResponse").val());
-         if(response == ""){
-           alert("Give some response");
-           return;
-           }
-
-        App.UserData[App.State.CurrentStage]=response;
-        $("#iResponse").val("");
+    SubmitInput() {
+        var response = $.trim($("#iResponse").val());
+        if (response == "") {
+            alert("Give some response");
+            return;
+        }
+        if ((App.State.CurrentStage == 1)||(App.State.CurrentStage == 6)){
+            this.addNew();
+        }
+        else{
+            App.UserData[App.State.CurrentStage] = response;
+            $("#iResponse").val("");
+        }
         PreviewPane.refresh();
         $("#userdataentrybox").hide();
         $("#previewviewarea").show();
         MainButtons.enableNextButton();
+
     },
-    show(){
+    show() {
         $("#iQuestion").text(this.Questions[App.State.CurrentStage]);
         $("#userdataentrybox").show();
+        if ((App.State.CurrentStage == 1)||(App.State.CurrentStage == 6))
+            $("#InputNewBtn").prop('disabled', false);
+        else
+            $("#InputNewBtn").prop('disabled', true);
+    },
+    addNew() {
+        var response = $.trim($("#iResponse").val());
+        if (response == "") {
+            alert("Give some response");
+            return;
+        }
+        App.UserData[App.State.CurrentStage].push(response);
+        $("#iResponse").val("");
     }
-
 }
 
 var PreviewPane =
 {
-  refresh()
-  {
-    controls = ["#p1","#p2","#p3","#p4","#p5","#p6","#p7","#p8","#p9"];
-    for(var i = 0 ; i<controls.length ; i++)
-      $(controls[i]).text(App.UserData[i]);
+    refresh() {
+        controls = ["#p1", "#p2", "#p3", "#p4", "#p5", "#p6", "#p7", "#p8", "#p9"];
+        for (var i = 0; i < controls.length; i++)
+            $(controls[i]).text(App.UserData[i]);
 
-  }
+    }
 }
 var MainButtons =
 {
-    "buttons" : ["#b1","#b2","#b3","#b4","#b5","#b6","#b7","#b8","#b9","#b10"],
-    disableCurrentButton()
-    {
+    "buttons": ["#b1", "#b2", "#b3", "#b4", "#b5", "#b6", "#b7", "#b8", "#b9", "#b10"],
+    disableCurrentButton() {
         id = this.buttons[App.State.CurrentStage];
         $(id).prop('disabled', true);
     },
-    enableNextButton()
-    {
+    enableNextButton() {
         App.State.CurrentStage++;
         id = this.buttons[App.State.CurrentStage];
         $(id).prop('disabled', false);
     },
 
-    collectresponse(btn){
+    collectresponse(btn) {
         this.disableCurrentButton();
         // show edit pare
         // hide preview pare
