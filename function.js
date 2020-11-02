f07c3818fe5f247431f05c036bb57638e6cbeb7d
mukeshyadav <mukeshyadav20000229@gmail.com>
2020-11-02 09:42:42 +0530

    Support data entry for dependent lists

--- function.js
+++ function.js
@@ -1,11 +1,11 @@
 var App =
 {
     "State": {
-        "CurrentStage": 0
+        "CurrentStage": 2
     },
     "UserData": [
-        "",
-        [],
+        "This is the problem",
+        ["choice 1" , "choice 2" , "choice 3" , "choice 4"],
         [],
         [],
         [],
@@ -15,9 +15,11 @@ var App =
         ""
     ]
 }
+$( document ).ready(function() {
+    MainButtons.enableCurrentButton();
+    PreviewPane.refresh();
 
-
-
+});
 var DataEntryPane =
 {
     "Questions": [
@@ -31,9 +33,18 @@ var DataEntryPane =
         "What is your Decision ?",
         "Assess Decision"
     ],
-    "IsList": [ false, true, true, true, true , false, false , false , false],
+    "IsList": [ false, true, false, false, false , false, false , false , false],
     "pivot": 1,
     "dependentlist": [ false, false, true, true, true , false, false , false , false],
+     "incr" : 0,
+
+    /**
+     * If this is a list type
+     * and this is not a pivot
+     * find out how many items should be there in the list
+     * 
+     * when it is the last item, show submit, instead of add more
+     */
     SubmitInput() {
         var response = $.trim($("#iResponse").val());
         if (response == "") {
@@ -48,6 +59,12 @@ var DataEntryPane =
             App.UserData[App.State.CurrentStage] = response;
             $("#iResponse").val("");
         }
+        if(this.dependentlist[App.State.CurrentStage])
+        {
+            App.UserData[App.State.CurrentStage].push(response);
+            this.incr=0;
+        $("#iResponse").val("");
+        }
         PreviewPane.refresh();
         $("#userdataentrybox").hide();
         $("#previewviewarea").show();
@@ -55,12 +72,35 @@ var DataEntryPane =
 
     },
     show() {
+
         $("#iQuestion").text(this.Questions[App.State.CurrentStage]);
         $("#userdataentrybox").show();
         if (this.IsList[App.State.CurrentStage])
             $("#InputNewBtn").prop('disabled', false);
         else
             $("#InputNewBtn").prop('disabled', true);
+            if(this.dependentlist[App.State.CurrentStage])
+            {
+             $("#InputNextBtn").prop('disabled', false);
+             $("#s4").prop('disabled', true);
+            }
+
+
+    },
+    showNext(){
+        var response = $.trim($("#iResponse").val());
+        if (response == "") {
+            alert("Give some response");
+            return;
+        }
+        this.incr++;
+        if(App.UserData[this.pivot].length-1==this.incr)
+        {
+            $("#InputNextBtn").prop('disabled', true);
+             $("#s4").prop('disabled', false);
+        }
+        App.UserData[App.State.CurrentStage].push(response);
+        $("#iResponse").val("");
     },
     addNew() {
         var response = $.trim($("#iResponse").val());
@@ -88,6 +128,10 @@ var MainButtons =
     disableCurrentButton() {
         id = this.buttons[App.State.CurrentStage];
         $(id).prop('disabled', true);
+    },
+    enableCurrentButton() {
+        id = this.buttons[App.State.CurrentStage];
+        $(id).prop('disabled', false);
     },
     enableNextButton() {
         App.State.CurrentStage++;
